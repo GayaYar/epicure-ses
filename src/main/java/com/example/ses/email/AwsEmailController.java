bbd20cf3299d6ef09772fdd49e0c9d022bd03d7d
GAYA <Gayay@academy-skills.com>
2022-06-30 12:31:25 +0300

    post email object

--- src/main/java/com/example/ses/email/AwsEmailController.java
+++ src/main/java/com/example/ses/email/AwsEmailController.java
@@ -1,10 +1,12 @@
 package com.example.ses.email;
 
+import com.example.ses.model.Email;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,9 +18,9 @@ public class AwsEmailController {
     private AwsEmailSender service;
 
     @PostMapping("/admin")
-    public ResponseEntity<Void> messageAdmin(@RequestParam String subject,@RequestParam String body) {
-        service.messageAdmin(subject, body);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Email> messageAdmin(@RequestBody Email email) {
+        service.messageAdmin(email.getSubject(), email.getBody());
+        return ResponseEntity.ok(email);
     }
 
 }
---
+++ src/main/java/com/example/ses/model/Email.java
@@ -0,0 +1,9 @@
+package com.example.ses.model;
+
+import lombok.Data;
+
+@Data
+public class Email {
+    private String subject;
+    private String body;
+}
